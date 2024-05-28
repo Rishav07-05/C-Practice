6420e226a37791b566a950fd5a137dbd9c2b4945
Rishav <rishavkumar18902@gmail.com>
2024-05-28 22:52:25 +0530

    Files Modified

---
+++ 4_if_Else.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a==b){
+        printf("Both the elements are same :) ");
+    }
+    else {
+        printf("Both elements are not same :( ");
+    }
+
+}
