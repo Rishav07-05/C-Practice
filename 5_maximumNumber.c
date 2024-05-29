e5686b92b3f23c890373d575de8fc0ce7ce3ab06
Rishav <rishavkumar18902@gmail.com>
2024-05-29 22:45:45 +0530

    File added

---
+++ 5_maximumNumber.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main(){
+    int a , b;
+    scanf("%d %d",&a , &b);
+    if( a > b){
+        printf("A is greater !!!");
+    }
+    else if( a < b){
+        printf("B is greater !!!");
+    }
+    else if( a == b){
+        printf("Both are equal !!!");
+    }
+    
+
+}
